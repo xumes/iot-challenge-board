18a77ce42eed79d094e6d29c33196210bcd2771a
walter cantori <wrlc@walters-mbp.br.ibm.com>
2018-06-28 11:06:18 -0300

    add components

---
+++ src/client/components/card/card.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+    render() {
+        return (
+            <div class="card">
+                <div class="number">13</div>
+            </div>
+        );
+    }
+}
+export default Card;
--- src/client/containers/Login/Login.js
+++ src/client/containers/Login/Login.js
@@ -3,33 +3,23 @@ import socket from '../../services/socket';
 
 class Login extends Component {
   state = {
-    name: 'name test',
-    game: ''
-  }
 
-  componentDidMount(){
-    socket.on('newVote', this.handleGetVotes);
-  }
-
-  testJoin = () => {
-    socket.emit('joinGame', this.state.game);
-  }
-
-  testVote = () => {
-    socket.emit('vote', {game: this.state.game, email: 'wrlc@br.ibm.com'});
-  }
-
-  handleGetVotes = (data) => {
-    console.log(data)
   }
 
   render () {
     return (
       <div>
         <h1>Login Page</h1>
-        <input value={this.state.game} onChange={(e) => this.setState({game: e.target.value })}/>
-        <button onClick={this.testJoin}>{this.state.name}</button>
-        <button onClick={this.testVote}>Vote</button>
+        <input type="text" placeholder="Name" /> 
+        <input type="text" placeholder="Team" /> 
+        <input type="checkbox" />
+
+        <input type="text" placeholder="Room Name" />         
+        <button>Create new Game</button>
+
+        <input type="text" placeholder="Room ID" />         
+        <button>Enter Game</button>
+        
       </div>
     )
   }
