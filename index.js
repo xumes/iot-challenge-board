28abbc6b5d3837c93c334c25a471c09cb31a98f7
walter cantori <wrlc@walters-mbp.br.ibm.com>
2018-06-28 17:34:41 -0300

    sockets

--- index.js
+++ index.js
@@ -20,17 +20,39 @@ app.use((req, res, next) => {
 });
 
 io.sockets.on('connection', (socket) => {
-  socket.on('joinGame', (game) => {
-    socket.join(game);
+  socket.on('joinGame', (data) => {
+    socket.join(data.game);
+    io.sockets.in(data.game).emit('newUser', data);
+  });
+
+  socket.on('userUpdate', (data) => {
+    io.sockets.in(data.game).emit('updatedUsers', data);
   });
 
   socket.on('leaveGame', (game) => {
     socket.leave(game);
   });
 
+  socket.on('addIssue', (data) => {
+    io.sockets.in(data.game).emit('newIssue', data.issue);
+  })
+
+  socket.on('sendIssues', (game) => {
+    io.sockets.in(game).emit('sendIssuesHost');
+  })
+
+  socket.on('issueslist', (data) => {
+    io.sockets.in(data.game).emit('getIssueslist', data.issues);
+  })
+
+  socket.on('sendCurrentIssue', (data) => {
+    io.sockets.in(data.game).emit('getCurrentIssue', data.currentIssue);
+  })
+
   socket.on('vote', (data) => {
     io.sockets.in(data.game).emit('newVote', data);
   });
+
 });
 
 app.use('/api', require('./src/server/routes'));
