11232d85f13f13c68ba8958e74de1c52e0fa11ae
Reginaldo Santos <rmsantos@br.ibm.com>
2018-11-15 23:18:41 -0200

    connecting Refresh component to the Action

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -1,11 +1,22 @@
-const baseUrl = 'https://iot-challenge-api.eu-de.mybluemix.net'
+import axios from 'axios'
 
-// /getdata    /getalerts
+const BASE_URL = 'https://iot-challenge-api.eu-de.mybluemix.net'  // /getdata    /getalerts
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
+export const FETCH_ALERTS = 'FETCH_ALERTS'
 
 export function fetchWeather(){
+    const url = `${BASE_URL}/getdata`
+    const request = axios.get(url)
     return {
         type: FETCH_WEATHER
+    }
+}
+
+export function fetchAlerts(){
+    const url = `${BASE_URL}/getalerts`
+    const request = axios.get(url)
+    return {
+        type: FETCH_ALERTS
     }
 }
--- src/client/containers/Refresh.js
+++ src/client/containers/Refresh.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { fetchWeather, fetchAlerts } from './../actions'
 
 class Refresh extends Component {
+    constructor(props) {
+        super(props)
+
+        this.onRefreshClick = this.onRefreshClick.bind(this)
+    }
+
+    onRefreshClick(event) {
+        event.preventDefault()
+    }
+
     render() {
         return (
             <div>
-                <button className='btn btn-primary btn-lg btn-block'>Refresh</button>
-                
+                <button onClick={this.onRefreshClick} className='btn btn-primary btn-lg btn-block'>Refresh</button>
             </div>
         )
     }
 }
 
-export default Refresh
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchWeather, fetchAlerts }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(Refresh)
