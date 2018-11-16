6fdc62dc0a2092f38e07545ae4565a20812c640d
Reginaldo Santos <rmsantos@br.ibm.com>
2018-11-16 00:31:08 -0200

    implementing Alert reducer

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
 import WeatherReducer from './reducer_weather'
+import AlertReducer from './reducer_alert'
 
 const rootReducer = combineReducers({
-    weather: WeatherReducer
+    weather: WeatherReducer,
+    alert: AlertReducer
 })
 
 export default rootReducer
---
+++ src/client/reducers/reducer_alert.js
@@ -0,0 +1,18 @@
+import _ from 'lodash'
+import { FETCH_ALERTS } from "../actions";
+
+const INITIAL_STATE = []
+
+export default function (state = INITIAL_STATE, action) {
+    if (action.type == FETCH_ALERTS) {
+        const result = _.chain(action.payload.data)
+            .uniqBy('payload')
+            .sortBy('date')
+            .reverse()
+            .slice(0, 5)
+            .value()
+        return result
+    } else {
+        return state
+    }
+}
